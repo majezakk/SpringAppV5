b40d5b42d63d4a88ef52740cd2084028f4c7290e
zakk_pc <zakhar.dm.ivanov@gmail.com>
2024-09-17 23:45:29 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringAppV5/SpringAppV5Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppV5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppV5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringAppV5/SpringAppV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppV5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
